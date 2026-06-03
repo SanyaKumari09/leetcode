89021542b6c1eb5edef5db184c7a5d3c6bc8e977
Sanya Kumari <219838739+SanyaKumari09@users.noreply.github.com>
2026-06-03 23:41:39 +0530

    Time: 24 ms (76.52%), Space: 49 MB (34.58%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -0,0 +1,70 @@
+class Solution{
+    public int subarraySum(int nums[],int k){
+        int n=nums.length;
+        int currSum=0;
+        int count=0;
+        HashMap<Integer, Integer> map= new HashMap<>();
+        map.put(0,1);
+        for(int i=0; i<n; i++){
+            currSum+=nums[i];
+            int find = currSum-k;
+            if(map.containsKey(find)){
+                count = count+ map.get(find);
+            }
+            map.put(currSum, map.getOrDefault(currSum,0)+1);
+        }
+        return count;
+    }
+}   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution{
+//     public int subarraySum(int nums[],int k){
+//         int n=nums.length;
+//         int prefixSum=0;
+//         int count=0;
+//         HashMap<Integer,Integer>map=new HashMap<>();
+//         map.put(0, 1);
+//         for(int i=0;i<n;i++){
+//             prefixSum+=nums[i];
+//             int remove=prefixSum-k;
+//             if(map.containsKey(remove)){
+//                 count+=map.get(remove);
+//             }
+//             map.put(prefixSum, map.getOrDefault(prefixSum,0)+1);
+//         }
+//         return count;
+//     }
+// }
+
+// // class Solution {
+// //     public int subarraySum(int[] nums, int k) { 
+// //         int n=nums.length;
+// //         int maxLen=0;
+// //         int count=0;
+// //         for(int i=0;i<n;i++){
+// //            int sum=0;
+// //            for(int j=i;j<n;j++){
+// //               sum+=nums[j];
+// //               if(sum==k){
+// //                 count++;
+// //                 maxLen=Math.max(maxLen,j-i+1);
+// //               }
+// //            }
+// //         }
+// //         return count;
+// //     }
+// // }
