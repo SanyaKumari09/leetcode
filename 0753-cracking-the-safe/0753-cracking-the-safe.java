ff027ded5f1c64ce44f415f670934be89446c6aa
Sanya Kumari <219838739+SanyaKumari09@users.noreply.github.com>
2026-06-25 22:24:10 +0530

    Time: 17 ms (20.72%), Space: 47.3 MB (19.37%) - LeetHub

---
+++ 0753-cracking-the-safe/0753-cracking-the-safe.java
@@ -0,0 +1,37 @@
+class Solution {
+
+    Set<String> visited = new HashSet<>();
+    StringBuilder ans = new StringBuilder();
+
+    public String crackSafe(int n, int k) {
+
+        StringBuilder start = new StringBuilder();
+
+        for (int i = 0; i < n - 1; i++) {
+            start.append('0');
+        }
+
+        dfs(start.toString(), k);
+
+        ans.append(start);
+
+        return ans.toString();
+    }
+
+    private void dfs(String node, int k) {
+
+        for (int i = 0; i < k; i++) {
+
+            String edge = node + i;
+
+            if (!visited.contains(edge)) {
+
+                visited.add(edge);
+
+                dfs(edge.substring(1), k);
+
+                ans.append(i);
+            }
+        }
+    }
+}
