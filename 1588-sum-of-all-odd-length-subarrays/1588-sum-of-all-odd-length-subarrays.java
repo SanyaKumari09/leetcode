e1ca6504be2597c10de2fb7d2e78aed2e6dcc906
Sanya Kumari <219838739+SanyaKumari09@users.noreply.github.com>
2026-08-01 15:43:17 +0530

    Time: 0 ms (100%), Space: 42.8 MB (95.83%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.java
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.java
@@ -1,30 +1,45 @@
 class Solution {
     public int sumOddLengthSubarrays(int[] arr) {
-        int n=arr.length;
-        int answer=0;
-        int s=0;
-        int[] prefSum = new int[n];
-        for(int i=0;i<n;i++){
-            s+=arr[i];
-            prefSum[i]=s;
-        }
-    
-        int sum=0;
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                int len=j-i+1;
+       int n=arr.length;
+       int answer=0;
+       for(int i=0;i<n;i++){
+        int left=i+1;
+        int right=n-i;
+        int total=left*right;
+        int oddCnt=(total+1)/2;
+        answer+=arr[i]*oddCnt;
+       }
 
-                if(len%2!=0){
-                    if(i==0){
-                        sum=prefSum[j];
-                    }else{
-                        sum=prefSum[j]-prefSum[i-1];
-                    }
-                    answer+=sum;
-                }
-            }
-        }
-
-        return answer;
+       return answer;
     }
 }
+
+
+
+
+// int n=arr.length;
+//         int answer=0;
+//         int s=0;
+//         int[] prefSum = new int[n];
+//         for(int i=0;i<n;i++){
+//             s+=arr[i];
+//             prefSum[i]=s;
+//         }
+    
+//         int sum=0;
+//         for(int i=0;i<n;i++){
+//             for(int j=i;j<n;j++){
+//                 int len=j-i+1;
+
+//                 if(len%2!=0){
+//                     if(i==0){
+//                         sum=prefSum[j];
+//                     }else{
+//                         sum=prefSum[j]-prefSum[i-1];
+//                     } 
+//                     answer+=sum;
+//                 }
+//             }
+//         }
+
+//         return answer;
