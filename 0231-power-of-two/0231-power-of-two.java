064024515be05adfccd93ac5bcdd227ad8655b8e
Sanya Kumari <219838739+SanyaKumari09@users.noreply.github.com>
2026-08-10 14:16:31 +0530

    Time: 1 ms (96.63%), Space: 42.5 MB (74.41%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+
+        return n > 0 && (n & (n - 1)) == 0;
+        // if(n<=0){
+        //     return false;
+        // }
+
+        // while(n%2 == 0){
+        //     n=n/10;
+        // }
+
+        // return n == 1;
+    }
+}
